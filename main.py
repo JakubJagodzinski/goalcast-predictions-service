d8054e8c0284204a444bbe7d8a927fe9ad1f51b5
JakubJagodzinski <j.jagodzinski.priv@gmail.com>
2026-01-18 15:15:50 +0100

    Add logging configuration

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import logging
 
 from events import prediction_request_event_consumer
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
 
 async def main():
     await prediction_request_event_consumer.consume_and_predict()
